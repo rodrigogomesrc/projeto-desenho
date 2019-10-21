7fd41df35cb1424f112f89e9561a8f2fb7876f78
RODRIGO GOMES DA ROCHA <rodrigogomesrc@A307-28L.imd.local>
2019-10-21 20:19:30 -0300

    leitura do arquivo

--- Desenho.c
+++ Desenho.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-	
+	FILE *file;
+	file = fopen("input.txt", "r");
+	char text[50];
+
+	if(file == NULL){
+		printf("File not found\n");
+
+	} else {
+
+		while(fgets(text, 50, file) != NULL){
+
+			printf("%s", text);
+		}
+		
+		fclose(file);
+	}
 	return 0;
 }
